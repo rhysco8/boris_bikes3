d7fb53281018b3ad18579781fd584b8988c95f17
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-05-30 14:48:13 +0100

    refactors first RSpec test

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,6 +1,4 @@
 require 'docking_station'
 describe DockingStation do
-  it "responds to release_bike" do
-    expect(DockingStation).to respond_to :release_bike
-  end
+  it { is_expected.to respond_to(:release_bike) }
 end
