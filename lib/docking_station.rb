dd584c23ddbddd778c8ba971d1ba538098ed56aa
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-05-30 16:14:51 +0100

    adds unit test for dock method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,10 @@
+require './lib/bike.rb'
+
 class DockingStation
   def release_bike
     Bike.new
+  end
+  def dock(bike)
+    bike
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,4 +5,11 @@ describe DockingStation do
     bike = docking_station.release_bike
     expect(bike.working?).to eq(true)
   end
+  # it 'docks a bike' do
+  #   docking_station = DockingStation.new
+  #   bike = docking_station.release_bike
+  #   docking_station.dock(bike)
+  #   expect(docking_station.bike).to eq()
+  # end
+  it { is_expected.to respond_to(:dock).with(1).argument }
 end
